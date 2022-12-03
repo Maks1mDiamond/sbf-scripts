d235535ab3131f53f2ec8615cb51e4bcd622e040
Maks1mDiamond <109898159+Maks1mDiamond@users.noreply.github.com>
2022-12-03 20:26:38 +0500

    changed keybind for quick delete script

--- Source Hotkey Menu GUI.lua
+++ Source Hotkey Menu GUI.lua
@@ -1012,7 +1012,7 @@ end)
 UIS.InputBegan:Connect(function(key, isChatting)
     if gui ~= nil then
         if isChatting then return end
-        if key.KeyCode == Enum.KeyCode.PageDown then
+        if key.KeyCode == Enum.KeyCode.End then
             k0,k1,k2,k3,k4,k5,k6,k7,k8,k9 = nil,nil,nil,nil,nil,nil,nil,nil,nil,nil
             gui:Destroy()
         end
